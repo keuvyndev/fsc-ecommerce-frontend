17c685009756a13c6cadd89b212ef03a00dcebb0
keuvyntdev <keuvyntdev@gmail.com>
2025-01-28 14:19:24 -0300

    feat: add theme colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+   primary: '#212529',
+   text:{
+      white: '#F8F9FA',
+      dark: '#343A40',
+   },
+   background:{
+      dark: '#212529',
+      white: '#F8F9A',
+   },
+   input: {
+      background: '#E9ECEFF',
+      placeholder: '#6C757D'
+   },
+   error: '#FF6A6A'
+}
+
+export default Colors;
