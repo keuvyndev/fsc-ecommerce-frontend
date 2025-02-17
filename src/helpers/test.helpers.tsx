47f2b2a76fe4a981d99ea0843c0aafad23adab79
keuvyntdev <keuvyntdev@gmail.com>
2025-02-17 17:14:40 -0300

    feat: add renderWithRedux helper function

---
+++ src/helpers/test.helpers.tsx
@@ -0,0 +1,36 @@
+import rootReducer from "../store/root-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { RootState } from "../store/store";
+
+// Helper para instanciar um teste envolvendo redux
+// Permite passar parâmetros de estado pré-carregado
+
+export const renderWithRedux = (
+   component: React.ReactElement, 
+   {
+      preloadedState, 
+      store = configureStore({
+         reducer: rootReducer,
+          preloadedState
+         }), 
+         ... renderOptions
+      }: {
+         preloadedState: RootState,
+         store?: any,
+      }
+   ) => {
+      const Wrapper = ({children}: {children: React.ReactElement}) => {
+         return (
+            <BrowserRouter>
+               <Provider store={store}>
+                  {children}
+               </Provider>
+            </BrowserRouter>
+         )
+      }
+
+      return render(component, {wrapper   : Wrapper, ...renderOptions})
+   }
