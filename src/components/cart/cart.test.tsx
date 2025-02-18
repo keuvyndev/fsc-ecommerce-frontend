09d4aca0bbaa234e926da215b8f100c27bc4f289
keuvyntdev <keuvyntdev@gmail.com>
2025-02-17 23:03:35 -0300

    test: ensure cart shows correct products

---
+++ src/components/cart/cart.test.tsx
@@ -0,0 +1,26 @@
+import { renderWithRedux } from "../../helpers/test.helpers"
+import Cart from "./cart.component"
+
+describe('Cart' , () => {
+   it('should show correct cart product', () => {
+      const {getByText} = renderWithRedux(<Cart />, {
+         preloadedState: {
+            cartReducer: {
+               products: [{
+                  id: '1',
+                  imageUrl: 'image_url',
+                  name: 'Boné',
+                  price: 100,
+                  quantity: 2
+               }]
+            }
+         } as any 
+      })
+
+      getByText(/boné/i)
+      getByText('R$ 100,00')
+      getByText('2')
+      getByText('Total: R$ 200')
+   })
+   
+})
