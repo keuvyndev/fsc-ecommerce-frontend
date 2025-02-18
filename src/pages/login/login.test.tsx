3ed0d83e2660427743c122d09247b17694195509
keuvyntdev <keuvyntdev@gmail.com>
2025-02-18 00:22:32 -0300

    test: ensure login shows error if email or password is invalid

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,6 +1,9 @@
 import userEvent from "@testing-library/user-event"
 import { renderWithRedux } from "../../helpers/test.helpers"
 import LoginPage from "./login.page"
+import * as firebaseAuth from "firebase/auth"
+
+jest.mock('firebase/auth')
 
 describe('Login', () => {
 
@@ -20,4 +23,39 @@ describe('Login', () => {
       userEvent.click(submitButton)
       await findByText(/por favor, insira um e-mail válido./i)
    })
+
+   it('should show an error if email is not found', async () => {
+      const mockFirebaseAuth = firebaseAuth as any
+
+      mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() => 
+         Promise.reject({code: firebaseAuth.AuthErrorCodes.USER_DELETED})
+      )
+
+      const {getByPlaceholderText, findByText, getByText} = renderWithRedux(<LoginPage />, {})
+      const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+      userEvent.type(emailInput, 'lorem@ipsum.com')
+      const passwordInput = getByPlaceholderText(/digite sua senha/i)
+      userEvent.type(passwordInput, '12345678')
+      const submitButton = getByText('Entrar')
+      userEvent.click(submitButton)
+      await findByText(/o e-mail não foi encontrado/i)
+   })
+
+   it('should show an error if password is not valid', async () => {
+      const mockFirebaseAuth = firebaseAuth as any
+
+      mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() => 
+         Promise.reject({code: firebaseAuth.AuthErrorCodes.INVALID_PASSWORD})
+      )
+
+      const {getByPlaceholderText, findByText, getByText} = renderWithRedux(<LoginPage />, {})
+      const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+      userEvent.type(emailInput, 'lorem@ipsum.com')
+      const passwordInput = getByPlaceholderText(/digite sua senha/i)
+      userEvent.type(passwordInput, '123456')
+      const submitButton = getByText('Entrar')
+      userEvent.click(submitButton)
+      await findByText(/A senha é inválida./i)
+   })
+
 });
