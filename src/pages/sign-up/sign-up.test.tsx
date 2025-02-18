67646284d29b611378183b4496e6c15a64e78c70
keuvyntdev <keuvyntdev@gmail.com>
2025-02-18 00:30:48 -0300

    test: esnure sign up shows error if password and password confirmation are different

--- src/pages/sign-up/sign-up.test.tsx
+++ src/pages/sign-up/sign-up.test.tsx
@@ -24,4 +24,18 @@ describe('Sign Up', () => {
 
       await findByText(/por favor, insira um e-mail válido./i)
    })
+
+   it('should show error when password and password confirmation are different', async () => {
+      const {getByText, findByText, getByPlaceholderText} = renderWithRedux(<SignUpPage />, {})
+      
+      
+      const passwordInput = getByPlaceholderText(/digite sua senha/i)
+      const passwordConfirmationInput = getByPlaceholderText(/digite sua senha novamente/i)
+      userEvent.type(passwordInput,'123456')
+      userEvent.type(passwordConfirmationInput,'12345678')
+      const submitButton = getByText('Criar Conta', {selector: 'button'})
+      userEvent.click(submitButton)
+
+      await findByText(/a confirmação de senha precisa ser igual a senha./i)
+   })
 })
